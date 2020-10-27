5dea307c4ed66726ca67c84b9d135ce23df14ad7
Involute1 <L_noth@yahoo.de>
2020-10-27 15:26:28 +0100

    works now with 3 jars

--- app/controllers/DominionController.scala
+++ app/controllers/DominionController.scala
@@ -1,18 +1,24 @@
 package controllers
 
+import com.google.inject.{Guice, Injector}
+import de.htwg.wt.dominion.controller.IController
+import de.htwg.wt.dominion.controller.maincontroller.Controller
+import de.htwg.wt.dominion.{CardMain, Dominion, DominionModule, PlayerMain}
 import javax.inject.Inject
 import play.api.mvc.{AbstractController, ControllerComponents}
 
 class DominionController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
 
-//  val dominionServer: Unit = Dominion.main(null);
-//  val playerServer: Unit  = PlayerMain.main(null);
-// val cardServer: Unit  = CardMain.main(null);
+  val dominionObj = Dominion
 
-//  val injector: Injector = Guice.createInjector(new DominionModule);
-//  val dominionController: IController = injector.getInstance(classOf[Controller])
+  val injector: Injector = Guice.createInjector(new DominionModule);
+  val dominionController: IController = injector.getInstance(classOf[Controller])
 
-  val dominionAsText: String = "asd"
+  val dominionServer: Unit = dominionObj.main(null);
+  val playerServer: Unit  = PlayerMain.main(null);
+  val cardServer: Unit  = CardMain.main(null);
+
+  val dominionAsText: String = dominionController.getControllerMessage
 
   def index = Action {
     Ok{views.html.index()}
--- build.sbt
+++ build.sbt
@@ -4,14 +4,18 @@ version := "1.0"
       
 lazy val `dominionplay` = (project in file(".")).enablePlugins(PlayScala)
 
-resolvers += "scalaz-bintray" at "https://dl.bintray.com/scalaz/releases"
+//resolvers += "scalaz-bintray" at "https://dl.bintray.com/scalaz/releases"
       
-resolvers += "Akka Snapshot Repository" at "https://repo.akka.io/snapshots/"
+//resolvers += "Akka Snapshot Repository" at "https://repo.akka.io/snapshots/"
       
 scalaVersion := "2.13.3"
 
-libraryDependencies ++= Seq( jdbc , ehcache , ws , specs2 % Test , guice )
+libraryDependencies ++= Seq(guice)
+//libraryDependencies ++= Seq("org.slf4j" % "slf4j-nop" % "1.7.26")
+//libraryDependencies ++= Seq("ch.qos.logback" % "logback-classic" % "1.1.3")
+//libraryDependencies += "org.scalatest" %% "scalatest" % "3.0.4" % "test"
+libraryDependencies ++= Seq("org.scalatestplus.play" %% "scalatestplus-play" % "5.0.0" % Test)
 
-unmanagedResourceDirectories in Test <+=  baseDirectory ( _ /"target/web/public/test" )  
+//unmanagedResourceDirectories in Test <+=  baseDirectory ( _ /"target/web/public/test" )
 
       
