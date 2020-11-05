42002090f087e4c68d2a1263408a6a8ff0d88b3b
Involute1 <L_noth@yahoo.de>
2020-11-05 13:52:23 +0100

    js done

--- public/javascripts/hello.js
+++ public/javascripts/hello.js
@@ -4,3 +4,12 @@ for (let i = 0; i < card_name.length; i++) {
     card_name[i].src = card_name[i].src + ".png"
     console.log(card_name[i].src)
 }
+
+function cardClick(cardElement) {
+    console.log(cardElement.id);
+    cardId = cardElement.id.substr(cardElement.id.indexOf("_") + 1);
+    console.log(cardId)
+
+    var loc = window.location;
+    window.location = "http://localhost:9000/dominion/process?input=" + cardId;
+}
