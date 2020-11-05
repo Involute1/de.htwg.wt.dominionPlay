28bf67fabeeec54a71796a1032057629acbe21fe
tobias-schoch <tobias@schochwebdesign.de>
2020-11-06 00:16:08 +0100

    jQuery finished. Ajax & jSON still missing.

--- public/javascripts/hello.js
+++ public/javascripts/hello.js
@@ -1,50 +1,45 @@
-var card_name = document.getElementsByClassName("card_name");
-var phase = document.getElementById("phase").innerHTML;
-var phaseType = phase.substr(phase.indexOf(" ") + 1);
-var img_cards_buydeck = document.getElementById("playing-decks").getElementsByClassName("card_name");
-var img_cards_handdeck = document.getElementById("hand-decks").getElementsByClassName("card_name");
-var buydeck = document.getElementById("playing-decks");
-var handdeck = document.getElementById("hand-decks");
+jQuery(document).ready(function ($) {
 
-changeImageSrc()
-allowedClicks()
+    changeImageSrc();
+    allowedClicks();
 
-function changeImageSrc() {
-    for (let i = 0; i < card_name.length; i++) {
-        card_name[i].src = card_name[i].src + ".png";
+    function changeImageSrc() {
+        $('.card_name').each(function () {
+            var src = $(this).attr('src');
+            $(this).attr('src', src + '.png')
+        });
     }
-}
 
-function allowedClicks() {
-    if (phaseType === "Buyphase") {
-        for (let i = 0; i < img_cards_buydeck.length; i++) {
-            img_cards_buydeck[i].style.cursor = "cursorurl";
-
+    function allowedClicks() {
+        var phase = document.getElementById("phase").innerHTML;
+        var phaseType = phase.substr(phase.indexOf(" ") + 1);
+        if (phaseType === "Buyphase") {
+            $('#hand-decks .card_name').each(function () {
+                $(this).css('cursor', 'not-allowed');
+            });
+            $('#playing-decks .card_name').each(function () {
+                $(this).css('cursor', 'cursorurl');
+            });
+            $('#hand-decks').css("background-image", "url(http://localhost:9000/assets/images/bg.png)");
+        } else if (phaseType === "Actionphase") {
+            $('#hand-decks .card_name').each(function () {
+                $(this).css('cursor', 'cursorurl');
+            });
+            $('#playing-decks .card_name').each(function () {
+                $(this).css('cursor', 'not-allowed');
+            });
+            $('#playing-decks').css("background-image", "url(http://localhost:9000/assets/images/bg.png)");
         }
-        for (let i = 0; i < img_cards_handdeck.length; i++) {
-            img_cards_handdeck[i].style.cursor = "not-allowed";
-        }
-        handdeck.style.backgroundImage = "url(http://localhost:9000/assets/images/bg.png)"
-
-    } else if (phaseType === "Actionphase") {
-        for (let i = 0; i < img_cards_buydeck.length; i++) {
-            img_cards_buydeck[i].style.cursor = "not-allowed";
-        }
-        for (let i = 0; i < img_cards_handdeck.length; i++) {
-            img_cards_handdeck[i].style.cursor = "cursorurl";
-        }
-        buydeck.style.backgroundImage = "url(http://localhost:9000/assets/images/bg.png)"
     }
-}
 
+    function cardClick(cardElement) {
+        var cardId = cardElement.id.substr(cardElement.id.indexOf("_") + 1);
+        var cardType = cardElement.id.substr(0, cardElement.id.indexOf('_'));
 
-function cardClick(cardElement) {
-    var cardId = cardElement.id.substr(cardElement.id.indexOf("_") + 1);
-    var cardType = cardElement.id.substr(0, cardElement.id.indexOf('_'));
-
-    if (phaseType === "Buyphase" && cardType === "card") {
-        window.location = "http://localhost:9000/dominion/process?input=" + cardId;
-    } else if (phaseType === "Actionphase" && cardType === "handCard") {
-        window.location = "http://localhost:9000/dominion/process?input=" + cardId;
+        if (phaseType === "Buyphase" && cardType === "card") {
+            window.location = "http://localhost:9000/dominion/process?input=" + cardId;
+        } else if (phaseType === "Actionphase" && cardType === "handCard") {
+            window.location = "http://localhost:9000/dominion/process?input=" + cardId;
+        }
     }
-}
+});
